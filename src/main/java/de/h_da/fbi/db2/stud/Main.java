e9804acaa5f5351baed259666cd02f920d056b87
PrivatePupu <manu.stolze93@yahoo.de>
2018-04-24 14:24:02 +0200

    Now I can work with this

---
+++ src/main/java/de/h_da/fbi/db2/stud/Main.java
@@ -0,0 +1,17 @@
+package de.h_da.fbi.db2.stud;
+import java.net.URISyntaxException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Main Class.
+ * @version 0.1.1
+ * @since 0.1.0
+ * @author A. Hofmann
+ * @author B.-A. Mokroß
+ */
+public class Main {
+    
+}
